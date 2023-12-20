b3b0200972e9bce5230352b5300a956841d494cd
Lapki <113145063+K1r1llLapshin@users.noreply.github.com>
2023-12-20 22:43:46 +0200

    Update UserRepository.h

--- Semester3/OOP/laba7/laba7/UserRepository.h
+++ Semester3/OOP/laba7/laba7/UserRepository.h
@@ -5,9 +5,9 @@ class UserRepository
 public:
 
 	virtual User* getById(int id) = 0;
-	virtual User* getByName(string name) = 0;
-	virtual User* getByEmail(string email) = 0;
-	virtual vector<User*> getByCity(string city) = 0;
+	virtual User* getByName(const string& name) = 0;
+	virtual User* getByEmail(const string& email) = 0;
+	virtual vector<User*> getByCity(const string& city) = 0;
 	virtual vector <User*> getByGender(Gender gender) = 0;
 
 };
